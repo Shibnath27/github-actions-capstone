bb5ad6d8e8d43e8aef919d0066612110d8697e78
Shibnath27 <shibnathdas27x@gmail.com>
2026-03-24 22:44:52 +0530

    modified docker & app.py

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,6 @@ COPY . .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
+EXPOSE 5000
 
-CMD ["python", "app.py"]
+CMD ["gunicorn","--bind", "0.0.0.0:5000", "app:app"]
--- app.py
+++ app.py
@@ -11,7 +11,3 @@ def home():
 @app.route("/health")
 def health():
     return 'Server is up and running'
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
